501a0cc48175b243dd6de237f109c2f3377c0eb5
Horváth Zoltán <horvath.zoltan.mobil@gmail.com>
2023-01-16 19:26:55 +0100

    1

--- models/model.h
+++ models/model.h
@@ -14,6 +14,23 @@ namespace Model
         QString toString() const;
     };
 
+    struct InsoleType{
+        static const QString CSV;
+        int Id;
+        QDateTime LastModified;
+        QString Name;
+        int InsoleGenderId;
+        int InsoleAgeCategoryId;
+        int InsoleSideId;
+        qreal EUSize;
+        QString GeometryCSV;
+        int R;
+        int VMax;
+        int VMin;
+
+        static QList<InsoleType> ParseList(const QString& str);
+        static InsoleType Parse(const QString& str);
+    };
 //    struct Data{
 //            //ApiVer apiVer;
 //            Device device;
